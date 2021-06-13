00db323a2fe67eb7d788ef30e80bc4545b8ed516
saemmilee <saemmi.lee@gmail.com>
2021-06-14 00:25:37 +0900

    Add modification and deletion to Q&A

--- 30days/pybo/views.py
+++ 30days/pybo/views.py
@@ -74,3 +74,41 @@ def question_create(request):
         context = {'form': form}
         return render(request, 'pybo/question_form.html', context)
 
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """
+    질문 수정
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    # 현재 사용자와 글을 쓴 사람이 다를 시 다시 해당 게시글로 redirect
+    if request.user != question.author:
+        return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == "POST":
+        # 화면으로 전달받은 입력값들을 덮어써서 QuestionForm을 생성하라는 의미
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.create_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        # instance 매개변수에 question을 지정하면 기존 값을 폼에 채울 수 있음
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    """
+    질문 삭제
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    # 현재 사용자와 글을 쓴 사람이 다를 시 다시 해당 게시글로 redirect
+    if request.user != question.author:
+        return redirect('pybo:detail', question_id=question.id)
+    question.delete()
+    return redirect('pybo:index')
